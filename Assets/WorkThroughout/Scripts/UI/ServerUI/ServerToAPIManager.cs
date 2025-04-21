e559806ec2d3f2123363c75e20acd7153f0810ed
Hyunjae Jeon <trewq4786@naver.com>
2025-04-22 00:24:10 +0900

    UI 디자인 시험 적용
    코드 리팩토링 전 안전빵 커밋

--- Assets/WorkThroughout/Scripts/UI/ServerUI/ServerToAPIManager.cs
+++ Assets/WorkThroughout/Scripts/UI/ServerUI/ServerToAPIManager.cs
@@ -772,4 +772,20 @@ public class ServerToAPIManager : MonoBehaviour
     {
         public int isInGame;
     }
+    [System.Serializable]
+    public class PlayerAddResponse
+    {
+        public bool success; // 요청 성공 여부
+        public int playerId; // MySQL에서 자동 생성된 playerId
+        public PlayerData playerData;
+
+        public PlayerAddResponse() { }
+
+        public PlayerAddResponse(bool success, int playerId, PlayerData playerData)
+        {
+            this.success = success;
+            this.playerId = playerId;
+            this.playerData = playerData;
+        }
+    }
 }
